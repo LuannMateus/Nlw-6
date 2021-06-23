643b4e19f4a8d015163e8359d8da2d67dc2a6fd2
LuannMateus <luanmateuslima34@gmail.com>
2021-06-23 16:34:19 -0300

    Day one and two / Flutter trail - Create Splash page, Login page and Home page struct

---
+++ payflow/lib/modules/splash/splash_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Image.asset(AppImages.union),
+          ),
+          Center(
+            child: Image.asset(AppImages.logoFull),
+          )
+        ],
+      ),
+    );
+  }
+}
